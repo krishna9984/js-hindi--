2ddd00258308789661dc7f1563317187cddcbeb1
KRISHNA MOHAN YADAV <152725365+krishna9984@users.noreply.github.com>
2026-01-30 07:41:45 +0000

    conversion looks

---
+++ conversion-operation.js
@@ -0,0 +1,27 @@
+ //let score= "33abc"// [gives nan]its not a pure number but it convert in the value number function without error in valdnumber 
+// find the type in the backend 
+// let score= null// gives zero
+let score= true;//1 output
+//console.log(typeof score);
+//console.log(typeof(score));
+
+
+let valueInNumber = Number(score) // convert the value of score int string to number 
+//console.log(typeof valueInNumber);
+//console.log( valueInNumber);//NAN 
+
+
+let isloggedIn = "krishna" // agar yaha koi value rahegi to true nhi to false milega 
+
+let booleanIsLoggedIn = Boolean(isloggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1=> true; 0= false
+// " " => false
+//"hitesh" => true 
+
+let someNumber = 33 
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log( typeof stringNumber);
+// gives the value string 33 and string 
