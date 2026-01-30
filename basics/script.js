4d46b9ea9cfb15763e34cad458189a2c3106e600
KRISHNA MOHAN YADAV <152725365+krishna9984@users.noreply.github.com>
2026-01-30 07:05:22 +0000

    varibles basics

---
+++ basics/script.js
@@ -0,0 +1,2 @@
+ console.log("krishna");
+//  variablesd and cocnstant 
---
+++ var.js
@@ -0,0 +1,23 @@
+ const accountId = 123123
+ let accountEmail="kriashna@gmasil.com"
+ var accountPassword= "12345"
+ accountCity="ballia "
+
+ // key words 
+ // const ka mtlb hota hye ki abb is value ko change nhi kiyasn jaa sakata hye yeah permanently hoo gye hye
+
+ //accountId=2 not allowed 
+
+ accountEmail= " krewfkhuh@gmail";
+ accountPassword="2342"
+ accountCity= "london"
+ let accountState;// it gives undefined beacuse there is no defined value 
+ console.log(accountId);
+ // console.table se ham ek hii bar me sare value ko together call kar sakate hye 
+ console.table([accountEmail,accountId,accountPassword,accountCity, accountState]);
+
+
+ //intila days me js scope par kam nhi karti par abb karti hye isiliye ham let aur var ko easily use karte hye aur jyada tar const aur let use karte for modern days 
+ // we mostly only use let and const together 
+ //ham javasacript me bina varible ko define kare value dee sakte hye pawr vo accha nhi 
+
