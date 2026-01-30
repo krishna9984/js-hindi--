1c071ee3c5eb953d7a57f1397038f4848d91e908
KRISHNA MOHAN YADAV <152725365+krishna9984@users.noreply.github.com>
2026-01-30 07:21:46 +0000

    data basics

---
+++ datatypes.js
@@ -0,0 +1,28 @@
+ " use strict ";// treat all js code as newer version 
+
+ //alert("hello"); // isko ham abhi use nhi akr sakte hye kyukii yeah abhi function nhi krega yaha 
+  // we are using node js not browser it only comes from the browser 
+
+// code must be in readable and clean for best scenarious 
+
+let name = " krishna"
+let age = 32
+let isloggedIn = false // boolean 
+let state;
+// numbers => 2 to the power 53
+// bigint its only use for bifg website 
+
+// strings => ""
+// bollean => it gives only true and false 
+
+// null => standalone value 
+
+// undefined => 
+    //symbol=> it mostly uses for uniquenes 
+
+// objects 
+
+console.log(typeof undefined);// undefined 
+console.log(typeof null);// in coated form null gives string value
+//null gives the value of object and undefined gives undefined 
+
