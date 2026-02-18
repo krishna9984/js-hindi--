6492e2291719162cc54c60e4138e0c252bdea3de
KRISHNA MOHAN YADAV <152725365+krishna9984@users.noreply.github.com>
2026-02-18 07:50:21 +0000

    stack primitive and non-primitive

---
+++ stack.js
@@ -0,0 +1,24 @@
+ // stack( only use as primitive places), heap ( non- primitive )
+
+//  let myname = "kriashna.com";
+
+// let anothername = "aswani kumar ";
+// anothername="abcd";
+
+// console.log(anothername);
+// console.log(myname);
+
+// let userOne = {
+//     email: "user@google.com",
+//     upi:"user@ybl"
+
+// }
+
+// let userTwo = userOne ;
+
+// userTwo.email = "KRISNA";
+
+// console.log(userTwo.email);
+// console.log(userOne.email);
+
+
