d98f4efeaddb905a2c22ac54ad7622ccf4282962
KRISHNA MOHAN YADAV <152725365+krishna9984@users.noreply.github.com>
2026-02-19 09:13:35 +0000

    slice and splice

---
+++ array1.js
@@ -0,0 +1,32 @@
+ //array
+
+  const myArr = [0,1,2,3,4,5,6,7] 
+//  const myheroes =["sahruk","peter"];
+//  console.log(myArr[3]);// we fetch the value by index
+// array methods
+
+// myArr.push(12);// ADDING TO THE LAST 
+// myArr.pop();// removing from last value of array
+// myArr.unshift(10);
+// myArr.shift();
+// console.log(myArr.includes(7));// it check the present value insisde the aary 
+// console.log(myArr.indexOf(3));
+// const newarr = myArr.join();
+// console.log(typeof newarr);
+// console.log(myArr);
+
+
+// slice or splice 
+
+console.log("A", myArr);
+const myn1 = myArr.slice(1,3);
+
+console.log(myn1);
+console.log("B", myArr);
+
+const myn2 = myArr.splice(1,3);
+console.log("C", myArr);
+console.log(myn2);
+
+
+
