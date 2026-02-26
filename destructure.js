72be11e1a9fa9bd9383bbc51413c2b6c053a86b8
KRISHNA MOHAN YADAV <152725365+krishna9984@users.noreply.github.com>
2026-02-26 08:16:57 +0000

    json format API

---
+++ destructure.js
@@ -0,0 +1,38 @@
+ const course = {
+    coursename: "js hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+
+ }
+
+ //course.courseInstructor
+
+ const {courseInstructor} = course
+
+ console.log(courseInstructor);
+
+
+
+ const navbar = ({comapany}) => { // destruc herre the comapny call
+ 
+ }
+
+ navbar(comapany = "hitesh")
+
+
+ // -----------    API CONCEPTS   ------
+// JSON
+//  {
+//   "name": "HITESH",
+//   "coursename" : "js hindi",
+//   "price" : "free"
+
+//  }
+
+// array format api
+
+[
+    {},
+    {},
+    {},
+]
