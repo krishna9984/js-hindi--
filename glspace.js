1da9cc1d18c8e0b1443a32b7937b809337633153
KRISHNA MOHAN YADAV <152725365+krishna9984@users.noreply.github.com>
2026-02-27 10:41:38 +0000

    global space and local or block space { }

---
+++ glspace.js
@@ -0,0 +1,28 @@
+//GLOBAL AND LOCAL SCOPE VARIABLE 
+// let a = 10
+
+// const b = 20
+
+// var c = 30
+
+//{} it define the scope curly braces
+let a = 300;
+
+if (true)   {
+let a = 10
+
+const b = 20
+
+var c = 30
+
+console.log("inner : ",a)
+
+}
+
+
+
+//console.log(a);
+//console.log(b);
+console.log(c);
+
+
