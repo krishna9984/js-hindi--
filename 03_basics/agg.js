8f8a25e2aadada9a3bc01d045af322ef3079d335
KRISHNA MOHAN YADAV <152725365+krishna9984@users.noreply.github.com>
2026-08-01 08:48:09 +0000

    operations discuss

---
+++ 03_basics/agg.js
@@ -0,0 +1,39 @@
+  // ----OPERATIONS -----//
+
+ let value = 3
+ let negValue = -value 
+  console.log(negValue)
+
+  console.log(2+2)// its operations to do any calculations 
+
+  let str1 = ' hello'
+  let str2= ' krishan '
+  let str3= str1+ str2
+  console.log(str3)
+
+  // complex situations **************
+
+
+console.log("1" + 1)// string plus number op= 11
+console.log (1 + "2")// 12 op
+ console.log("1" +2 + 3 ) // 123 op
+
+  console.log(1 + 2 + "4") // 34 op why == agar sting first hye to sabse pehle string hoga sara and agar number hye to pehle operation hoga then other string
+
+  console.log(+true)//=1 
+  console.log(true+1)
+
+let num1, num2, num3
+ num1 = num2= num3= 3+3
+
+ console.log(num1);
+
+let gameCounter = 100;
+++gameCounter;// op increase hoke print hota hye  101
+
+console.log(gameCounter);
+
+
+
+
+
